f824690e91bd5d50f8b782037d9d59d24ba07915
NahuelFredes <nahuelfredes2018@gmail.com>
2018-08-14 06:05:31 -0300

    s

---
+++ OrganizadorRenta/src/OrganizadorRenta/Administracion.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package OrganizadorRenta;
+
+/**
+ *
+ * @author nahuelfredes
+ */
+public class Administracion extends javax.swing.JFrame {
+
+    private MainMenu menu;
+
+    public Administracion() {
+        initComponents();
+        this.menu = new MainMenu();
+
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        btn_volverAdmin = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        btn_volverAdmin.setText("Volver");
+        btn_volverAdmin.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_volverAdminActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(47, 47, 47)
+                .addComponent(btn_volverAdmin, javax.swing.GroupLayout.PREFERRED_SIZE, 361, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(65, Short.MAX_VALUE)
+                .addComponent(btn_volverAdmin, javax.swing.GroupLayout.PREFERRED_SIZE, 158, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(77, 77, 77))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btn_volverAdminActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_volverAdminActionPerformed
+       cerrarAdmin();
+    }//GEN-LAST:event_btn_volverAdminActionPerformed
+
+    public void cerrarAdmin() {
+        this.setVisible(false);
+        menu.setVisible(true);
+        
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btn_volverAdmin;
+    // End of variables declaration//GEN-END:variables
+
+}
